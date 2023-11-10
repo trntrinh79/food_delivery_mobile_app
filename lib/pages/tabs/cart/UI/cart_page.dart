af569f0557148a7ce9ee694392c5f917eea6d159
trntrinh79 <trntrinh79@gmail.com>
2023-11-10 15:24:17 +0700

    Passing Data to Cart

--- lib/pages/tabs/cart/UI/cart_page.dart
+++ lib/pages/tabs/cart/UI/cart_page.dart
@@ -1,13 +1,30 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:food_delivery_mobile_app/cubit/app_cubit.dart';
 
-class CartPage extends StatelessWidget {
-  const CartPage({super.key});
+import 'package:food_delivery_mobile_app/cubit/cart_cubit/cubit/cart_cubit.dart';
+import 'package:food_delivery_mobile_app/utils/fonts.dart';
+import 'package:food_delivery_mobile_app/widget/cart_card.dart';
+
+class CartPage extends StatefulWidget {
+  const CartPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<CartPage> createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+  @override
+  void initState() {
+    super.initState();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<AppCubit, AppState>(
+    return BlocBuilder<CartCubit, CartState>(
       builder: (context, state) {
         if (state is CartLoadingState) {
           return const Scaffold(
@@ -17,20 +34,48 @@ class CartPage extends StatelessWidget {
           );
         }
         if (state is CartLoadedState) {
+          // var quantity = productDataModel.quantity;
+          // var price = productDataModel.price;
+          // var totalPrice = quantity * price;
+          // productDataModel.totalPrice = totalPrice;
+          var item = state.productList;
+          print("Updated Cart Page");
           return Scaffold(
             appBar: AppBar(
               backgroundColor: Colors.grey[50],
               elevation: 0,
               toolbarHeight: 0,
             ),
-            body: const SafeArea(
+            body: SafeArea(
               child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Column(
-                      children: [Text("Cart Page")],
+                    const Text(
+                      "Helooooooooo",
+                      style: AppFontStyle.HEADLINE_LARGE,
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * .7,
+                      child: ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: item.length,
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, int index) {
+                            bool last = item.length == (index + 1);
+                            return Padding(
+                              padding: EdgeInsets.only(right: last ? 0 : 20),
+                              child: GestureDetector(
+                                onTap: () {},
+                                child: Container(
+                                  child:
+                                      CartCard(productDataModel: item[index]),
+                                ),
+                              ),
+                            );
+                          }),
                     ),
                   ],
                 ),
